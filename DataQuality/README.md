cbac91668697c9280e8c2c69a782667c1940b225
guilhermecchacur <guilherme_cefaloni@hotmail.com>
2026-02-03 23:21:50 -0300

    feat: add quality step

---
+++ DataQuality/README.md
@@ -0,0 +1,26 @@
+# DataQuality
+
+Nessa etapa as validações de qualidade foram aplicadas sobre os dados da camada silver.
+Foi aplicado as seguintes validações:
+    . Quantidade de nulos em cada coluna
+    . QUantidade de chaves duplicadas na PK
+    . Quantidade de datas de atualização maiores que hoje
+
+O resultado do Quality fica salvo como tabela, que pode ser imcorporado ao lake para consultas ou relatórios, como mostrado abaixo:
++----------+--------------+----------+---------------------+--------------+-----+
+|table     |partition_date|total_rows|column               |metric        |value|
++----------+--------------+----------+---------------------+--------------+-----+
+|tb_cliente|2026-02-04    |397       |cod_cliente          |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |cod_cliente          |duplicated_key|0    |
+|tb_cliente|2026-02-04    |397       |nm_cliente           |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |nm_pais_cliente      |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |nm_cidade_cliente    |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |nm_rua_cliente       |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |num_casa_cliente     |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |num_telefone_cliente |nulls         |35   |
+|tb_cliente|2026-02-04    |397       |dt_nascimento_cliente|nulls         |0    |
+|tb_cliente|2026-02-04    |397       |dt_atualizacao       |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |dt_atualizacao       |future_date   |0    |
+|tb_cliente|2026-02-04    |397       |tp_pessoa            |nulls         |0    |
+|tb_cliente|2026-02-04    |397       |vl_renda             |nulls         |0    |
++----------+--------------+----------+---------------------+--------------+-----+
--- DataQuality/main.py
+++ DataQuality/main.py
@@ -4,7 +4,7 @@ import pyspark.sql.functions as F
 from pyspark.sql import DataFrame, Row
 from pyspark.sql.window import Window
 
-from utils import EtlConfig, Logger, BaseSpark, read_file, write_file
+from utils.utils import EtlConfig, Logger, BaseSpark, read_file, write_file, parse_args
 
 
 class Qualities:
@@ -41,31 +41,49 @@ class Qualities:
         df = df.groupby(column).agg(F.count(column).alias('qnt'))
         return df.filter(F.col('qnt') > 1).count()
     
+    @staticmethod
+    def future_updated(df: DataFrame, column: str) -> int:
+        """
+        Count records with invalid update dates (future dates).
+
+        Args:
+            df: Input Spark DataFrame.
+            column: Update date column to be validated.
+
+        Returns:
+            Number of records with update date greater than the current date.
+        """
+        return df.filter(F.col(column) > F.current_date()).count()
+        
 
 class QualityPipeline:
     """
     Pipeline responsible for executing data quality checks.
     """
 
-    def __init__(self):
+    def __init__(self, config):
         """
         Initialize pipeline configuration, logger and Spark session.
 
         Args:
-            None
+            config: config files for process parameters
 
         Returns:
             None
         """
         try:
-            self.config = EtlConfig()
+            self.config = config
             self.name: str = self.config.name
             self.log = Logger(name=self.name)
+
             self.spark = BaseSpark(name=self.name).create_session()
+            self.spark.conf.set('spark.sql.sources.partitionOverwriteMode', 'dynamic')
+            self.spark.conf.set('spark.sql.shuffle.partitions', '16')
+            self.spark.conf.set('spark.default.parallelism', '8')
 
         except Exception:
             try:
-                self.log.exception("Failed to initialize ETL pipeline.")
+                self.log.exception('Failed to initialize ETL pipeline.')
             except Exception:
                 pass
             raise
@@ -89,7 +107,7 @@ class QualityPipeline:
 
         df = read_file(
             spark=self.spark,
-            file_type="parquet",
+            file_type='parquet',
             path=partition_path
         )
 
@@ -98,6 +116,7 @@ class QualityPipeline:
 
         rows = []
         for c in df.columns:
+            self.log.info(f'Checking column {c}')
             nulls = Qualities.qnt_nulls(df=df, column=c)
 
             rows.append(Row(
@@ -105,7 +124,7 @@ class QualityPipeline:
                 partition_date=date_today,
                 total_rows=num_rows,
                 column=c,
-                metric="nulls",
+                metric='nulls',
                 value=int(nulls) if nulls is not None else None
             ))
 
@@ -120,8 +139,23 @@ class QualityPipeline:
                     partition_date=date_today,
                     total_rows=num_rows,
                     column=c,
-                    metric="duplicated_key",
+                    metric='duplicated_key',
                     value=int(dup) if dup is not None else None
+                ))
+
+            if c == self.config.update_column:
+                fut = Qualities.future_updated(
+                    df=df,
+                    column=self.config.update_column
+                )
+
+                rows.append(Row(
+                    table=self.config.tabela,
+                    partition_date=date_today,
+                    total_rows=num_rows,
+                    column=c,
+                    metric='future_date',
+                    value=int(fut) if fut is not None else None
                 ))
 
         quality_report_df = self.spark.createDataFrame(rows)
@@ -134,6 +168,17 @@ class QualityPipeline:
             partition_column='partition_date'
         )
 
+        self.log.info(f'Finished Quality Check for {self.config.tabela} in {date_today} partition')
+
+def main():
+    args = parse_args()
+
+    try:
+        config = EtlConfig(config_path=args.config)
+    except Exception as e:
+        raise e
+    
+    QualityPipeline(config=config).run()
 
 if __name__ == '__main__':
-    QualityPipeline().run()
+    main()
